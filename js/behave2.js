76e215a4b2b029f86a5faac1173ae3a9354142b8
VincentHokie <VincentHokie@users.noreply.github.com>
2015-05-27 16:46:30 +0300

    Update behave2.js

--- js/behave2.js
+++ js/behave2.js
@@ -6,12 +6,75 @@ $(document).ready(function() {
 	$("header li, footer img, #leadership td img").hover(function() {
 		$(this).css("cursor", "pointer");
 	});
+		var vision ="<strong>“To nurture and empower a Generation that values Human Rights as a platform for social welfare, peace, integrity, social justice, transformation and sustainable development in Uganda, Africa and globally.”</strong>";
+		var visionHead = "Vision";
+		var mission = "<strong>“To promote exchange of information and best practices by empower young individuals and their communities to be effective agents of change, human rights advocates and credible youths, leaders and successful professionals within their spheres of influence.”</strong>";
+		var missionHead = "Mission";
+		
+		setInterval(function() {
+		$("#leftStuff").toggleClass("active");
+			}, 9900);
+		
+		$("#leftStuff p").html(vision).css("background-color", "#0F75BC").css("color", "#000");
+		$("#leftStuff h4").html(visionHead);
+		setInterval(function() {
+		
+		if($("#leftStuff").hasClass("active")) {
+		$("#leftStuff p").html(mission).css("background-color", "#0F75BC").css("color", "#000");
+		$("#leftStuff h4").html(missionHead);
+		} else {
+		$("#leftStuff p").html(vision).css("background-color", "#0F75BC").css("color", "#000");
+		$("#leftStuff h4").html(visionHead);
+		}
+			}, 10000);
+			
+		setInterval(function() {
+			$("#inspiration, #family").toggleClass("nowShowing");
+			}, 26900);
+			
+		setInterval(function() {
+			
+		if ( $("#inspiration").hasClass("nowShowing") ) {
+			
+		$("#Malala").fadeToggle(500).delay(4000).fadeToggle(1000);
+		$("#mlk").delay(4500).fadeToggle(1000).delay(4000).fadeToggle(1000);
+		$("#NelsonM").delay(10000).fadeToggle(1000).delay(4000).fadeToggle(1000);
+		$("#shirin").delay(15500).fadeToggle(1000).delay(4000).fadeToggle(1000);
+		$("#silence").delay(21000).fadeToggle(1000).delay(4000).fadeToggle(1000);
+		
+		
+	} else if ( $("#family").hasClass("nowShowing") ) {
+		
+		$("#oneFam").fadeToggle(500).delay(4000).fadeToggle(1000);
+		$("#twoFam").delay(4500).fadeToggle(1000).delay(4000).fadeToggle(1000);
+		$("#threeFam").delay(10000).fadeToggle(1000).delay(4000).fadeToggle(1000);
+		$("#fourFam").delay(15500).fadeToggle(1000).delay(4000).fadeToggle(1000);
+		$("#fiveFam").delay(21000).fadeToggle(1000).delay(4000).fadeToggle(1000);
+	}
+		}, 27000);
+		
+		if ( $("#inspiration").hasClass("nowShowing") ) {
+			
+		$("#Malala").fadeToggle(500).delay(4000).fadeToggle(1000);
+		$("#mlk").delay(4500).fadeToggle(1000).delay(4000).fadeToggle(1000);
+		$("#NelsonM").delay(10000).fadeToggle(1000).delay(4000).fadeToggle(1000);
+		$("#shirin").delay(15500).fadeToggle(1000).delay(4000).fadeToggle(1000);
+		$("#silence").delay(21000).fadeToggle(1000).delay(4000).fadeToggle(1000);
+		
+		
+	}
 	
 	var bodyHeight = $(".active").height();
 	var screenHeight = $("html").innerHeight();
 	var screenWidth = $("html").innerWidth();
 	var halfHeight = screenHeight/4;
 	var halfWidth = screenWidth/3;
+	
+	if($(document).innerWidth() < 700) {
+		$("#home, #contacts, #leadership, #about").click(function() {
+			$("header").toggle(400);
+		});
+	}
 	
 	$("#home").addClass("active");
 	
@@ -34,20 +97,7 @@ $(document).ready(function() {
 	$("#home, #contacts, #leadership, #about").removeClass("active");
 	$("#about").addClass("active");
 	});
-	
-	$("#leadership td img").click(function() {
-		var image = $(this).attr("src");
-		$("#fadedBack").css("height", screenHeight).css("width", screenWidth).addClass("show");
-		$("#profile").css("top", halfHeight).css("left", halfWidth).addClass("show");
-		$("#profile img").attr("src", image);
-	});
-	
-	$("#profile, #fadedBack").click(function() {
-		$("#profile img").attr("src", "");
-		$("#profile p").html("");
-		$("#fadedBack").toggleClass("show");
-		$("#profile").toggleClass("show");
-	});
+
 	
 	var chair = "<p>Chairperson</p><p>Kasata Daniel</p><p>Ugandan</p><p>*Course*</p>";
 	var viceChair = "<p>Vice Chairperson</p><p>Edris Kitugi</p><p>Ugandan</p><p>*Course*</p>";
@@ -61,6 +111,23 @@ $(document).ready(function() {
 	var depSpeaker = "<p>Deputy Speaker</p><p>Kefa Adule</p><p>Ugandan</p><p>*Course*</p>";
 	var assPubSec = "<p>Assistant Publicity Secretary</p><p>Mohammad Abubakar Balde</p><p>Somali</p><p>*Course*</p>";
 	var assOrgSec = "<p>Assistant Oraganizing Secretary</p><p>Mwesigwa Freziric</p><p>Ugandan</p><p>*Course*</p>";
+	
+	if($(document).innerWidth() >= 700) {
+		
+	$("#leadership td img").click(function() {
+		var image = $(this).attr("src");
+		$("#fadedBack").css("height", screenHeight).css("width", screenWidth).fadeToggle(500);
+		$("#profile").css("top", halfHeight).css("left", halfWidth).fadeToggle(500);
+		$("#profile img").attr("src", image);
+	});
+	
+	$("#profile, #fadedBack").click(function() {
+		$("#profile img").attr("src", "");
+		$("#profile p").html("");
+		$("#profile").fadeToggle(1);
+		$("#fadedBack").fadeToggle(300);
+	});
+	
 	
 	$("#leadership td img").click(function() { $("#profile p").html(""); });
 	
@@ -88,13 +155,5 @@ $(document).ready(function() {
 	
 	$(".assOrgSec").click(function() { $("#profile p").html(assOrgSec); });
 	
-	  if($("#home").hasClass("active")) {
-		for(var i=0; i<=5; i++) {
-		$("#Malala").delay(4000).hide(1000);
-		$("#mlk").delay(9000).hide(1000);
-		$("#NelsonM").delay(14000).hide(1000);
-		$("#shirin").delay(19000).hide(1000);
-		$("#silence").delay(24000).hide(1000);
-	  }
 	}
 });
